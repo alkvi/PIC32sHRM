727d01b6211ae1560e552e6ffd210f6bec6737ed
Alex <akv@kth.se>
2017-11-18 18:06:36 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,104 @@
+/* mipslabmain.c
+
+   This file written 2015 by Axel Isaksson,
+   modified 2015, 2017 by F Lundevall
+   modified 2017 by akv
+
+   Latest update 2017-11-18 by akv
+
+   For copyright and licensing, see file COPYING */
+
+#include <stdint.h>   /* Declarations of uint_32 and the like */
+#include <pic32mx.h>  /* Declarations of system-specific addresses etc */
+#include "projecthead.h"  /* function declarations! */
+
+
+int main(void) {
+
+    /* This will set the peripheral bus clock to the same frequency
+	  as the sysclock. That means 80 MHz, when the microcontroller
+	  is running at 80 MHz. Changed 2017, as recommended by Axel. */
+	SYSKEY = 0xAA996655;  /* Unlock OSCCON, step 1 */
+	SYSKEY = 0x556699AA;  /* Unlock OSCCON, step 2 */
+	while(OSCCON & (1 << 21)); /* Wait until PBDIV ready */
+	OSCCONCLR = 0x180000; /* clear PBDIV bit <0,1> */
+	while(OSCCON & (1 << 21));  /* Wait until PBDIV ready */
+	SYSKEY = 0x0;  /* Lock OSCCON */
+	
+	/* Set up output pins */
+	AD1PCFG = 0xFFFF;
+	ODCE = 0x0;
+	TRISECLR = 0xFF;
+	PORTE = 0x0;
+	
+	/* Output pins for display signals */
+	PORTF = 0xFFFF;
+	PORTG = (1 << 9);
+	ODCF = 0x0;
+	ODCG = 0x0;
+	TRISFCLR = 0x70;
+	TRISGCLR = 0x200;
+	
+	/* Set up input pins */
+	TRISDSET = (1 << 8);
+	TRISFSET = (1 << 1);
+	
+	/* Set up SPI as master */
+	SPI2CON = 0;
+	SPI2BRG = 4;
+	/* SPI2STAT bit SPIROV = 0; */
+	SPI2STATCLR = 0x40;
+	/* SPI2CON bit CKP = 1; */
+    SPI2CONSET = 0x40;
+	/* SPI2CON bit MSTEN = 1; */
+	SPI2CONSET = 0x20;
+	/* SPI2CON bit ON = 1; */
+	SPI2CONSET = 0x8000;
+	
+	display_init();
+	display_string(0, "rad 0");
+	display_string(1, "rad 1");
+	display_string(2, "rad 2");
+	display_string(3, "rad 3");
+	display_update();
+	
+	display_image(96, icon);
+	
+	//initialize monitor things
+	asm("ei");
+	//setupADCManual();
+	setupADCAuto();
+
+
+  	//clear TMR2
+  	TMR2 = 0;
+  	//set scaling to 256:1 (bits 4 to 6 on). 
+  	T2CON = 0x70;
+  	//set period to 31 250 for 100 ms (with 80 MHz clock)
+  	PR2 = 31250;
+
+  	//clear interrupt flag (bit 8 in IFS0)
+  	IFS(0) &= (0 << 8);
+  	//set interrupt prios to highest (bits 2 3 4 in IPC2) (and IPC2 bits 0 and 1 for subprio)
+  	IPC(2) |= 0x1f;
+  	//enable timer 2 interrupt (bit 8 in IEC0)
+  	IEC(0) |= 0x100;
+
+  	//turn T2 on (bit 15)
+  	//T2CON |= (1 << 15);
+
+	while( 1 )
+	{
+		//do stuff
+		monitor();
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
---
+++ monitor.c
@@ -0,0 +1,98 @@
+/* monitor.c
+
+This file written 2017 by akv
+Latest update 2017-11-18
+
+*/
+
+#include <stdint.h>   /* Declarations of uint_32 and the like */
+#include <pic32mx.h>  /* Declarations of system-specific addresses etc */
+#include "projecthead.h"  /* function declarations! */
+
+int testval = 0;
+int rawVal = 0;
+
+//Interrupt services
+void user_isr( void ) {
+
+	//handle interrupt from ADC -- get new value from ADC buffer, clear flag
+	if ( (IFS(1) & 0x2) > 0 ){
+		rawVal = ADC1BUF0;
+	    IFSCLR(1) = (0x2);
+  }
+
+  	//handle interrupt from timer 2
+  	if ( (IFS(0) & 0x100) > 0 ){
+	    IFS(0) &= (0 << 8);
+  	}
+
+}
+
+
+int readPin(char pin){
+    AD1CHS = pin << 16;       		//select ADC pin in AD1CHS<16:19>
+    AD1CON1SET = 0x0002;           	//sample by setting SAMP bit
+    while( AD1CON1 & 0x0002 );      //wait until internal counter ends sampling
+    while( !AD1CON1 & 0x0001 );    	//wait for auto convert
+    return ADC1BUF0;                //get result
+}
+
+
+void monitor(void) {
+
+	char * rawConv = itoaconv(rawVal);
+	display_string(1, rawConv);
+	display_update();
+
+}
+
+void setupADCManual(void){
+
+	AD1PCFGCLR = (1 << 4);	//set up A1 as analog, not digital
+	TRISBSET = (1 << 4);	//initialize Port B so that bit 4 is set (use A1 as analog input)
+
+	AD1CON1CLR = 0x8000;    // make sure ADC is off before messing with it
+ 
+ 	//AD1CON1<2>		0 - ASAM (sample bit manually set to start sampling)
+	//AD1CON1<7:5>		111 - SSRC internal counter ends sampling and starts conversion (auto convert)
+    AD1CON1 = 0x00e0;       
+    AD1CON2 = 0;            // AD1CON2<15:13> set voltage reference to pins AVSS/AVDD
+    AD1CON3 = 0x0f01;       // TAD = 4*TPB, acquisition time = 15*TAD 
+
+    AD1CON1SET = 0x8000; 	//turn ADC back on 
+
+}
+
+void setupADCAuto(void){
+
+	AD1PCFGCLR = (1 << 4);	//set up A1 as analog, not digital
+
+    AD1CON1CLR = 0x8000;    // make sure ADC is off before messing with it
+
+    IPCSET(6) = (3 << 26);	//AD1IP
+    IPCSET(6) = (1 << 24);	//AD1IS
+    IFSCLR(1) = (0x2);		//AD1IF
+    IECSET(1) = (0x2);		//AD1IE
+
+	//AD1CON1<2>		1 - ASAM (sample bit automatically set)
+	//AD1CON1<7:5>		111 - SSRC internal counter ends sampling and starts conversion (auto convert)
+	AD1CON1SET = 0x00e4;
+
+	//AD1CON2<1>, 		0 - BUFM Buffer configured as 16 bit buffer (only one)
+    //AD1CON2<10>, 		0 - CSCNA  : Scan inputs
+    AD1CON2 = 0x0000;
+
+    //AD2CON2<5:2>		x - SMPI  : Interrupt flag set at after x completed conversions
+    AD1CON2SET = (1 << 2);
+
+    //AD1CON3<7:0> 		ADCS TAD = TPB * 2 * (ADCS<7:0> + 1) = 4 * TPB
+    //AD1CON3<12:8> 	SAMC AD1CON3<12:8> * TAD = 15 * TAD
+    AD1CON3 = 0x0f01;
+
+    AD1CHS = 4 << 16;	//ADC1CHS
+
+    AD1CON1SET = 0x8000; 	//turn ADC back on 
+
+}
+
+
